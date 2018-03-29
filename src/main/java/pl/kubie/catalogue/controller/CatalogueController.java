f92f7a96b1af063659a8b7fc45b3e77edfc379d1
panwoj <pan_woj@poczta.onet.pl>
2018-03-29 22:39:04 +0200

    Changed Controller - added autowired DbService

--- src/main/java/pl/kubie/catalogue/controller/CatalogueController.java
+++ src/main/java/pl/kubie/catalogue/controller/CatalogueController.java
@@ -1,19 +1,23 @@
 package pl.kubie.catalogue.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pl.kubie.catalogue.domain.ItemDto;
+import pl.kubie.catalogue.service.DbService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/catalogue")
 public class CatalogueController {
+    @Autowired
+    private DbService dbService;
+
     @RequestMapping(method = RequestMethod.GET, value = "items")
     public List<ItemDto> getItems() {
-        return new ArrayList<>();
+        return dbService.getAllItems();
     }
 }
