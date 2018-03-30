6c3ecbbbce91abeb70ce6cb912043c2558199ac7
panwoj <pan_woj@poczta.onet.pl>
2018-03-31 00:02:03 +0200

    Added repository

---
+++ src/main/java/pl/kubie/catalogue/repository/ItemRepository.java
@@ -0,0 +1,15 @@
+package pl.kubie.catalogue.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import pl.kubie.catalogue.domain.Item;
+
+import java.util.List;
+
+public interface ItemRepository extends CrudRepository<Item, Long> {
+
+    @Override
+    List<Item> findAll();
+
+    @Override
+    Item save(Item item);
+}
